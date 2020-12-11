53a151705d049616bfa2fec5f9c4079b2da2f7d0
ksyyeon <suyeonmin@naver.com>
2020-12-11 20:55:54 +0900

    dbp-final

--- db_con.php
+++ db_con.php
@@ -1,5 +1,5 @@
 <?php 
-    $link = mysqli_connect("localhost", "root", "20172026", "dbp");
+    $link = mysqli_connect("localhost", "dbpt07", "dbp2020!!", "dbpt07");
     
     if(mysqli_connect_errno()){
         echo "MySQL 접속에 실패했습니다. 관리자에게 문의하세요.";
